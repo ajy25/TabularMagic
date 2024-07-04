eeec54c4a873248889b65924539e176882b9b7b3
rzryanz00 <68756716+rzryanz00@users.noreply.github.com>
2024-07-03 23:28:00 -0400

    [feature] Started Frontend for GLM

--- tabularmagic/_src/interactive/__init__.py
+++ tabularmagic/_src/interactive/__init__.py
@@ -2,6 +2,7 @@ from .eda import ComprehensiveEDA
 from .regression.voteselectreg import RegressionVotingSelectionReport
 from .regression.mlreg import MLRegressionReport
 from .regression.linreg import LinearRegressionReport
+from .regression.glmreg import GLMRegressionReport
 from .classification.mlclass import MLClassificationReport
 
 
@@ -10,5 +11,6 @@ __all__ = [
     "RegressionVotingSelectionReport",
     "MLRegressionReport",
     "LinearRegressionReport",
+    "GLMRegressionReport",
     "MLClassificationReport",
 ]

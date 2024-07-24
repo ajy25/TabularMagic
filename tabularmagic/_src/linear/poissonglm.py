a141b6e6d51589719b3fa25079071a5486d864c4
rzryanz00 <68756716+rzryanz00@users.noreply.github.com>
2024-07-24 13:46:03 -0400

    [feature] added step to poisson GLM

--- tabularmagic/_src/linear/poissonglm.py
+++ tabularmagic/_src/linear/poissonglm.py
@@ -2,6 +2,46 @@ import statsmodels.api as sm
 import numpy as np
 from ..metrics.regression_scoring import RegressionScorer
 from ..data.datahandler import DataEmitter
+import pandas as pd
+from typing import Literal
+from ..utils import ensure_arg_list_uniqueness
+
+
+def score_poisson_model(
+    X_train: pd.DataFrame,
+    y_train: pd.DataFrame,
+    feature_list,
+    metric: Literal["aic", "bic"],
+) -> float:
+    """Calculates the AIC or BIC score for a given model.
+
+    Parameters
+    ----------
+    X_train : pd.DataFrame
+        The training data.
+    y_train : pd.Series
+        The training target.
+    feature_list : list[str]
+        The list of features to include in the model.
+    metric : str
+        The metric to use for scoring. Either 'aic' or 'bic'.
+
+    Returns
+    -------
+    float
+        The AIC or BIC score for the model.
+    """
+    if len(feature_list) == 0:
+        return np.inf
+
+    subset_X_train = X_train[feature_list]
+    new_model = sm.GLM(y_train, subset_X_train, 
+                       family = sm.families.Poisson()).fit(cov_type="HC3")
+    if metric == "aic":
+        score = new_model.aic
+    elif metric == "bic":
+        score = new_model.bic
+    return score
 
 
 class PoissonLinearModel:
@@ -76,6 +116,189 @@ class PoissonLinearModel:
             n_predictors=n_predictors,
             name=self._name,
         )
+
+    @ensure_arg_list_uniqueness()
+    def step(
+        self,
+        direction: Literal["both", "backward", "forward"] = "backward",
+        criteria: Literal["aic", "bic"] = "aic",
+        kept_vars: list[str] | None = None,
+        all_vars: list[str] | None = None,
+        start_vars: list[str] | None = None,
+        max_steps: int = 100,
+    ) -> list[str]:
+        """Finish writing description
+
+        Categorical variables will either be included or excluded as a whole.
+
+        Parameters
+        ----------
+        direction : Literal["both", "backward", "forward"]
+            The direction of the stepwise selection. Default: 'backward'.
+        criteria : Literal["aic", "bic"]
+            The criteria to use for selecting the best model. Default: 'aic'.
+        kept_vars : list[str]
+            The variables that should be kept in the model. Default: None.
+            If None, defaults to empty list.
+        all_vars : list[str]
+            The variables that are candidates for inclusion in the model. Default: None.
+            If None, defaults to all variables in the training data.
+        start_vars : list[str]
+            The variables to start the bidirectional stepwise selection with.
+            Ignored if direction is not 'both'. If direction is 'both' and
+            start_vars is None, then the starting variables are the kept_vars.
+            Default: None.
+        max_steps : int
+            The maximum number of steps to take. Default: 100.
+
+        Returns
+        -------
+        list[str]
+            The subset of predictors that are most likely to be significant.
+        """
+        if max_steps <= 0:
+            raise ValueError("max_steps cannot be non-positive")
+
+        X_train, y_train = self._dataemitter.emit_train_Xy()
+
+        # set upper to all possible variables if nothing is specified
+        if all_vars is None:
+            all_vars = X_train.columns.tolist()
+        if kept_vars is None:
+            kept_vars = []
+
+        # ensure that kept vars are in all vars
+        for var in kept_vars:
+            if var not in all_vars:
+                raise ValueError(f"{var} is not in all_vars")
+
+        # set our current variables to our starting list
+        if direction == "forward":
+            included_vars = kept_vars.copy()
+        elif direction == "backward":
+            included_vars = all_vars.copy()
+        elif direction == "both":
+            if start_vars is None:
+                included_vars = kept_vars.copy()
+            else:
+                included_vars = start_vars.copy()
+
+        # set our starting score and best models
+        current_score = score_poisson_model(
+            X_train,
+            y_train,
+            included_vars,
+            metric=criteria,
+        )
+        current_step = 0
+
+        while current_step < max_steps:
+            # Forward step
+            if direction == "forward":
+                excluded = list(set(all_vars) - set(included_vars))
+
+                best_score = current_score
+                var_to_add = None
+                for new_var in excluded:
+                    candidate_features = included_vars + [new_var]
+                    score = score_poisson_model(
+                        X_train,
+                        y_train,
+                        candidate_features,
+                        metric=criteria,
+                    )
+                    if score < best_score:
+                        best_score = score
+                        var_to_add = new_var
+
+                # If we didn't find a variable to add (score is not better), break
+                if var_to_add is None:
+                    break
+
+                included_vars.append(var_to_add)
+
+            # Backward step
+            elif direction == "backward":
+                if len(included_vars) <= len(kept_vars):
+                    break
+
+                best_score = current_score
+                var_to_remove = None
+
+                for candidate in included_vars:
+                    if candidate in kept_vars:
+                        continue
+
+                    candidate_features = included_vars.copy()
+                    candidate_features.remove(candidate)
+                    score = score_poisson_model(
+                        X_train,
+                        y_train,
+                        candidate_features,
+                        metric=criteria,
+                    )
+                    if score < best_score:
+                        best_score = score
+                        var_to_remove = candidate
+
+                if var_to_remove is None:
+                    break
+
+                included_vars.remove(var_to_remove)
+
+            elif direction == "both":
+                excluded = list(set(all_vars) - set(included_vars))
+
+                best_score = current_score
+
+                best_forward_score = current_score
+                var_to_add = None
+                for new_var in excluded:
+                    candidate_features = included_vars + [new_var]
+                    score = score_poisson_model(
+                        X_train,
+                        y_train,
+                        candidate_features,
+                        metric=criteria,
+                    )
+                    if score < best_forward_score:
+                        best_forward_score = score
+                        var_to_add = new_var
+
+                best_backward_score = current_score
+                var_to_remove = None
+
+                for candidate in included_vars:
+                    if candidate in kept_vars:
+                        continue
+
+                    candidate_features = included_vars.copy()
+                    candidate_features.remove(candidate)
+                    score = score_poisson_model(
+                        X_train,
+                        y_train,
+                        candidate_features,
+                        metric=criteria,
+                    )
+                    if score < best_backward_score:
+                        best_backward_score = score
+                        var_to_remove = candidate
+
+                if best_forward_score < best_backward_score:
+                    if var_to_add is None:
+                        break
+                    included_vars.append(var_to_add)
+                    best_score = best_forward_score
+                else:
+                    if var_to_remove is None:
+                        break
+                    included_vars.remove(var_to_remove)
+                    best_score = best_backward_score
+
+            current_score = best_score
+            current_step += 1
+
+        return included_vars
 
         def __str__(self):
             return self._name

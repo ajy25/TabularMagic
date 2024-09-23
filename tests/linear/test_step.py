ca46612ca5ead50659f2d87af1168888acb28e02
ajy25 <andrew_j_yang@brown.edu>
2024-09-23 18:45:34 -0400

    [debug] further testing of logistic regression stepwise selection

--- tests/linear/test_step.py
+++ tests/linear/test_step.py
@@ -37,7 +37,39 @@ def setup_data():
     df_ols = X
     df_ols["y"] = y
 
-    return {"df_ols": df_ols}
+
+
+    np.random.seed(42)
+    X = pd.DataFrame(
+        {
+            "x1": np.random.randn(100) * 0.5 + 10,  # Continuous variable
+            "x2": np.random.randn(100) * 3,  # Continuous variable (noise)
+            "cat1": np.random.choice(["A", "B", "C"], 100),  # Categorical variable
+            "cat2": np.random.choice(
+                ["W", "X", "Y", "Z"], 100
+            ),  # Categorical variable (noise)
+            "x3": np.random.randn(100) * 3,  # Continuous variable (noise)
+        }
+    )
+
+    # Create a binary target variable
+    logit = (
+        3 * X["x1"]
+        - 5 * (X["cat1"] == "A")
+        + 10 * (X["cat1"] == "B")
+        + np.random.randn(100)* 3 - 3
+    )
+    y = (logit > logit.median()).astype(int)
+
+    df_logistic = X
+    df_logistic["y"] = y
+    df_logistic["y"] = df_logistic["y"].astype(str)
+
+
+    return {
+        "df_ols": df_ols,
+        "df_logistic": df_logistic,
+    }
 
 
 def test_backward_selection_ols(setup_data):
@@ -80,8 +112,6 @@ def test_forward_selection_ols(setup_data):
 
     selected_features = reduced_lmreport.train_report()._X_eval_df.columns.tolist()
 
-    print(selected_features)
-
     # Assert that only the most important features are selected
     assert "x1" in selected_features  # Should be included
     assert "x2" not in selected_features  # Should be excluded
@@ -108,7 +138,35 @@ def test_both_direction_selection_ols(setup_data):
 
     selected_features = reduced_lmreport.train_report()._X_eval_df.columns.tolist()
 
-    print(selected_features)
+    # Assert that only the most important features are selected
+    assert "x1" in selected_features  # Should be included
+    assert "x2" not in selected_features  # Should be excluded
+    assert "x3" not in selected_features  # Should be excluded
+
+    # All cat2 levels should be excluded
+    # All cat1 levels should be included, with the exception of 'A' (we drop first)
+    for feature in selected_features:
+        if "cat2" in feature:
+            assert False
+        if "cat1" in feature:
+            assert feature != "cat1::A"
+
+
+def test_backward_selection_logistic(setup_data):
+    df = setup_data["df_logistic"]
+
+    analyzer = tm.Analyzer(df=df)
+    logistic_report = analyzer.glm(
+        family="binomial",
+        target="y",
+        predictors=["x1", "x2", "cat1", "cat2", "x3"],
+    )
+    reduced_logistic_report = logistic_report.step("backward")
+
+    selected_features = reduced_logistic_report.train_report()._X_eval_df.columns.tolist()
+
+    print(logistic_report.metrics())
+    print(reduced_logistic_report.metrics())
 
     # Assert that only the most important features are selected
     assert "x1" in selected_features  # Should be included
@@ -122,3 +180,65 @@ def test_both_direction_selection_ols(setup_data):
             assert False
         if "cat1" in feature:
             assert feature != "cat1::A"
+
+
+
+def test_both_direction_selection_logistic(setup_data):
+    df = setup_data["df_logistic"]
+
+    analyzer = tm.Analyzer(df=df)
+    logistic_report = analyzer.glm(
+        family="binomial",
+        target="y",
+        predictors=["x1", "x2", "cat1", "cat2", "x3"],
+    )
+    reduced_logistic_report = logistic_report.step("both")
+
+    selected_features = reduced_logistic_report.train_report()._X_eval_df.columns.tolist()
+
+    print(logistic_report.metrics())
+    print(reduced_logistic_report.metrics())
+
+    # Assert that only the most important features are selected
+    assert "x1" in selected_features  # Should be included
+    assert "x2" not in selected_features  # Should be excluded
+    assert "x3" not in selected_features  # Should be excluded
+
+    # All cat2 levels should be excluded
+    # All cat1 levels should be included, with the exception of 'A' (we drop first)
+    for feature in selected_features:
+        if "cat2" in feature:
+            assert False
+        if "cat1" in feature:
+            assert feature != "cat1::A"
+
+
+def test_forward_selection_logistic(setup_data):
+    df = setup_data["df_logistic"]
+
+    analyzer = tm.Analyzer(df=df)
+    logistic_report = analyzer.glm(
+        family="binomial",
+        target="y",
+        predictors=["x1", "x2", "cat1", "cat2", "x3"],
+    )
+    reduced_logistic_report = logistic_report.step("forward")
+
+    selected_features = reduced_logistic_report.train_report()._X_eval_df.columns.tolist()
+
+    print(logistic_report.metrics())
+    print(reduced_logistic_report.metrics())
+
+    # Assert that only the most important features are selected
+    assert "x1" in selected_features  # Should be included
+    assert "x2" not in selected_features  # Should be excluded
+    assert "x3" not in selected_features  # Should be excluded
+
+    # All cat2 levels should be excluded
+    # All cat1 levels should be included, with the exception of 'A' (we drop first)
+    for feature in selected_features:
+        if "cat2" in feature:
+            assert False
+        if "cat1" in feature:
+            assert feature != "cat1::A"
+
